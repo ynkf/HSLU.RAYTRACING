35007a2ae50c53333a729bc571bffff2515e006d
Yannick Frischknecht <yannick.frischknecht@leuchterag.ch>
2025-02-26 20:11:28 +0100

    implements Vector classes for 2d and 3d

---
+++ RayTracing/Vector2D.cs
@@ -0,0 +1,16 @@
+﻿namespace RayTracing;
+
+public class Vector2D(int X, int Y)
+{
+    public int X { get; } = X;
+    public int Y { get; } = Y;
+
+    public static Vector2D operator +(Vector2D a, Vector2D b) => new(a.X + b.X, a.Y + b.Y);
+
+    public static Vector2D operator -(Vector2D a, Vector2D b) => new(a.X - b.X, a.Y - b.Y);
+
+    public static Vector2D operator *(int scalar, Vector2D a) => new(scalar * a.X, scalar * a.Y);
+
+    public static Vector2D operator *(Vector2D a, int scalar) => scalar * a;
+}
+    
---
+++ RayTracing/Vector3D.cs
@@ -0,0 +1,16 @@
+﻿namespace RayTracing;
+
+public class Vector3D(int X, int Y, int Z)
+{
+    public int X { get; } = X;
+    public int Y { get; } = Y;
+    public int Z { get; } = Z;
+
+    public static Vector3D operator +(Vector3D a, Vector3D b) => new(a.X + b.X, a.Y + b.Y, a.Z + a.Z);
+
+    public static Vector3D operator -(Vector3D a, Vector3D b) => new(a.X - b.X, a.Y - b.Y, a.Z - b.Z);
+
+    public static Vector3D operator *(int scalar, Vector3D a) => new(scalar * a.X, scalar * a.Y, scalar * a.Z);
+
+    public static Vector3D operator *(Vector3D a, int scalar) => scalar * a;
+}
